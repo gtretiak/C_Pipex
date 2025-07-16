d9cd9c407ff22c5b358994545c590ccdb5d395fd
Georgii Tretiakov <george22021988@gmail.com>
2025-07-16 15:23:35 +0100

    Update processes_bonus.c

--- Sources_bonus/processes_bonus.c
+++ Sources_bonus/processes_bonus.c
@@ -22,27 +22,27 @@ void	ft_one_cmd(t_pipex *pipex)
 	if (pid == 0)
 		ft_run_single(pipex);
 	else
-		wait(NULL);
+		wait(NULL); // parent waiting for a child to finish
 	exit(0);
 }
 
 void	ft_run_single(t_pipex *pipex)
 {
 	pipex->infile = ft_get_infile(pipex);
-	if (pipex->infile != -1)
+	if (pipex->infile != -1) 
 	{
-		dup2(pipex->infile, 0);
+		dup2(pipex->infile, 0); // redirection input to STDIN
 		close(pipex->infile);
 	}
 	pipex->outfile = ft_get_outfile(pipex);
 	if (pipex->outfile != -1)
 	{
-		dup2(pipex->outfile, 1);
+		dup2(pipex->outfile, 1); // redirection output to STDOUT
 		close(pipex->outfile);
 	}
 	else
 		exit(2);
-	ft_execute(pipex->av[2 + pipex->check_lim], pipex);
+	ft_execute(pipex->av[2 + pipex->check_lim], pipex); // current process replacement with execution
 	close(1);
 }
 
@@ -51,14 +51,14 @@ void	ft_run_parent(t_pipex *pipex)
 	int	i;
 
 	i = 0;
-	while (i < pipex->num_cmd - 1)
+	while (i < pipex->num_cmd - 1) // parent closes all the ends since it doesn´t need them
 	{
 		close(pipex->fd[i][0]);
 		close(pipex->fd[i][1]);
 		i++;
 	}
 	i = 0;
-	while (i < pipex->num_cmd)
+	while (i < pipex->num_cmd) // then it waits for all children
 	{
 		waitpid(pipex->pid[i], NULL, 0);
 		i++;
@@ -67,11 +67,11 @@ void	ft_run_parent(t_pipex *pipex)
 
 void	ft_redirection(t_pipex *pipex)
 {
-	char	buf[4096];
+	char	buf[4096]; // buffer to copy the content
 	int		i;
 
-	pipex->infile = ft_get_infile(pipex);
-	pipex->outfile = ft_get_outfile(pipex);
+	pipex->infile = ft_get_infile(pipex); // openning infile
+	pipex->outfile = ft_get_outfile(pipex); // openning outfile
 	if (pipex->infile == -1 || pipex->outfile == -1)
 	{
 		if (pipex->infile == -1)
@@ -80,7 +80,7 @@ void	ft_redirection(t_pipex *pipex)
 			close(pipex->infile);
 		ft_handle_special_error(ERR_REDIR, 1);
 	}
-	while (1)
+	while (1) // actual copying (reading, then writing)
 	{
 		i = read(pipex->infile, buf, sizeof(buf));
 		if (i <= 0)
@@ -88,7 +88,7 @@ void	ft_redirection(t_pipex *pipex)
 		if (write(pipex->outfile, buf, i) == -1)
 			ft_putstr_fd("Error writing to outfile\n", 2);
 	}
-	close(pipex->infile);
+	close(pipex->infile); // closing...
 	close(pipex->outfile);
-	ft_remove_tmp(pipex);
+	ft_remove_tmp(pipex); // removind it redirection was performed from here_doc
 }
