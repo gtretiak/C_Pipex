954d7842d746f5a47703040f89926f5f5816797d
Georgii Tretiakov <george22021988@gmail.com>
2025-07-16 14:06:38 +0100

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -13,7 +13,7 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <fcntl.h>
+# include <fcntl.h> // for open(), O_RDONLY, O_CREAT, etc
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
