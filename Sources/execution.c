7dfe2edf2a3df52da4f036d3cdac168a888a6768
Georgii Tretiakov <george22021988@gmail.com>
2025-07-16 14:43:10 +0100

    Update execution.c

--- Sources/execution.c
+++ Sources/execution.c
@@ -17,19 +17,19 @@ void	ft_execute(char *arg, char **env)
 	char	**cmd_with_args;
 	char	*path;
 
-	cmd_with_args = ft_split(arg, ' ');
-	if (cmd_with_args[0] == NULL)
+	cmd_with_args = ft_split(arg, ' '); // "ls -l" becomes ["ls", "-l", NULL]
+	if (cmd_with_args[0] == NULL) // cleanup if the command is empty 
 		ft_free_split(cmd_with_args, 1);
-	if (access(cmd_with_args[0], F_OK) != 0
-		|| ft_strncmp(cmd_with_args[0], "/", 1))
-		path = ft_get_path(cmd_with_args[0], env);
+	if (access(cmd_with_args[0], F_OK) != 0 // if it exists...
+		|| ft_strncmp(cmd_with_args[0], "/", 1)) // ... and starts with / which means it´s absolute path, we can use it directly
+		path = ft_get_path(cmd_with_args[0], env); // otherwise we try look for it
 	else
 		path = cmd_with_args[0];
-	if (!path)
-		ft_free_split(cmd_with_args, 1);
-	if (execve(path, cmd_with_args, env) == -1)
+	if (!path) // invalid path, so cleanup
+		ft_free_split(cmd_with_args, 1); 
+	if (execve(path, cmd_with_args, env) == -1) // if execve fails, cleaning up
 	{
-		if (access(cmd_with_args[0], F_OK) != 0)
+		if (access(cmd_with_args[0], F_OK) != 0) // if path was malloced
 			free(path);
 		ft_free_split(cmd_with_args, 1);
 	}
@@ -41,13 +41,13 @@ char	**ft_find_paths(char **env)
 	char	**paths;
 
 	i = 0;
-	while (env[i])
+	while (env[i]) // iterating through the env list
 	{
 		if (ft_strnstr(env[i], "PATH=", 5))
 			break ;
 		i++;
 	}
-	if (!env[i])
+	if (!env[i]) // the end of the list with no PATH=
 		return (NULL);
 	paths = ft_split(env[i] + 5, ':');
 	return (paths);
@@ -61,13 +61,13 @@ char	*ft_get_path(char *cmd, char **env)
 	char	*pathname;
 
 	i = 0;
-	paths = ft_find_paths(env);
-	while (paths[i])
+	paths = ft_find_paths(env); // retrieving the list of PATH directories
+	while (paths[i]) // iterating through the list of PATHs
 	{
-		prepath = ft_strjoin(paths[i++], "/");
-		pathname = ft_strjoin(prepath, cmd);
+		prepath = ft_strjoin(paths[i++], "/"); // joining /
+		pathname = ft_strjoin(prepath, cmd); // joining cmd
 		free(prepath);
-		if (access(pathname, F_OK) == 0)
+		if (access(pathname, F_OK) == 0) // we return the constructed path exists if it exists
 		{
 			ft_free_split(paths, 0);
 			return (pathname);
